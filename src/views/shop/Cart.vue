8e49a8f6a3278be06c3f924f990e67a87f21ecce
Evgeny Smolyakov <esmolyakov@softproduct.pro>
2024-10-30 10:37:47 +0300

    #2

---
+++ src/views/shop/Cart.vue
@@ -0,0 +1,14 @@
+
+<script setup>
+
+</script>
+
+<template>
+  <div class="cart">
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/shop/Favorites.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="favorites">
+    Favorites
+  </div>
+</template>
+
+
+<style lang="scss" scoped>
+
+</style>
